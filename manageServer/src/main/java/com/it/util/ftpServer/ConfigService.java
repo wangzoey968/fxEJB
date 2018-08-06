ee94dd9a796c1d5c2944b86d10b14f4701469d93
wangzoey968 <1324633295@qq.com>
2018-08-06 16:01:52 +0800

    bug

--- manageServer/src/main/java/com/it/util/ftpServer/ConfigService.java
+++ manageServer/src/main/java/com/it/util/ftpServer/ConfigService.java
@@ -4,8 +4,13 @@ package com.it.util.ftpServer;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.springframework.core.io.FileSystemResource;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,11 +47,10 @@ public class ConfigService {
     public static void init() {
         try {
             SAXReader reader = new SAXReader();
-
-            String path = System.getProperty("webModuleRootDir") + "/src/main/java/com/it/util/ftpServer/ConfigServer.xml";
-            System.out.println(path);
-            
-            Document document = reader.read(new File(System.getProperty("webModuleRootDir") + "/src/main/java/com/it/util/ftpServer/ConfigServer.xml"));
+            URI uri = ConfigService.class.getClass().getClassLoader().getResource("ConfigServer.xml").toURI();
+            //URL url = Class.class.getClass().getResource("ConfigServer.xml");
+            //File file = Paths.get(System.getProperty("user.dir") + "/src/main/java/com/it/util/ftpServer/ConfigServer.xml").toFile();
+            Document document = reader.read(Paths.get(uri).toFile());
             //获取根节点元素对象
             Element root = document.getRootElement();
             List<Element> list = root.elements();
