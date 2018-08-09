91420193d2807eca67982f44946449208f68adce
wangzoey968 <1324633295@qq.com>
2018-08-09 11:20:48 +0800

    文件服务器修改配置

--- manageServer/src/main/java/com/it/util/ftpServer/FtpConfigService.java
+++ manageServer/src/main/java/com/it/util/ftpServer/FtpConfigService.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 此类重要作用是连接到文件服务器ftpServer,设置文件的缓存路径cache
+ * 此类作用是连接到文件服务器ftpServer,设置文件的缓存路径cache
  */
 public class FtpConfigService {
 
@@ -28,7 +28,6 @@ public class FtpConfigService {
         return getCfg("ftpPassword");
     }
 
-
     private static Map<String, String> cfgMap = new HashMap<>();
 
     public static String getCfg(String key) {
@@ -37,12 +36,11 @@ public class FtpConfigService {
 
     public static void init() {
         try {
-            //FtSys
             /*cfgMap.put("fileRoot","E:\\ft");
-            cfgMap.put("cache","E:\\ft\\cache");
-            cfgMap.put("ftpServer","ftp://vip.ft.com:8080");
-            cfgMap.put("ftpLoginName","f");
-            cfgMap.put("ftpPassword","fx_0");
+            cfgMap.put("cache","E:\\ftSys\\cache");
+            cfgMap.put("ftpServer","ftp://vip.ftn.com:10");
+            cfgMap.put("ftpLoginName","ft");
+            cfgMap.put("ftpPassword","ft_0");
             cfgMap.put("clientMustReg","false");*/
         } catch (Exception e) {
             e.printStackTrace();
