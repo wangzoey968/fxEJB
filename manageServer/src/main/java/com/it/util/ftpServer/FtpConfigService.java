837efe0e8345cc78c3aef9e9f51d87bf01098132
wangzoey968 <1324633295@qq.com>
2018-08-09 11:10:28 +0800

    bug

--- manageServer/src/main/java/com/it/util/ftpServer/FtpConfigService.java
+++ manageServer/src/main/java/com/it/util/ftpServer/FtpConfigService.java
@@ -37,24 +37,13 @@ public class FtpConfigService {
 
     public static void init() {
         try {
-            /*SAXReader reader = new SAXReader();
-            URI uri = FtpConfigService.class.getClass().getClassLoader().getResource("ConfigServer.xml").toURI();
-            //URL url = Class.class.getClass().getResource("ConfigServer.xml");
-            //File file = Paths.get(System.getProperty("user.dir") + "/src/main/java/com/it/util/ftpServer/ConfigServer.xml").toFile();
-            Document document = reader.read(Paths.get(uri).toFile());
-            //获取根节点元素对象
-            Element root = document.getRootElement();
-            List<Element> list = root.elements();
-            for (Element s : list) {
-                cfgMap.put(s.getName(), s.getText());
-                System.out.println(s.getName() + "/" + s.getText());
-            }*/
-            cfgMap.put("fileRoot","E:\\FactorySys");
-            cfgMap.put("cache","E:\\FactorySys\\cache");
-            cfgMap.put("ftpServer","ftp://vip.zhundianyinwu.com:21");
-            cfgMap.put("ftpLoginName","factory");
-            cfgMap.put("ftpPassword","factory_4006085466");
-            cfgMap.put("clientMustReg","false");
+            //FtSys
+            /*cfgMap.put("fileRoot","E:\\ft");
+            cfgMap.put("cache","E:\\ft\\cache");
+            cfgMap.put("ftpServer","ftp://vip.ft.com:8080");
+            cfgMap.put("ftpLoginName","f");
+            cfgMap.put("ftpPassword","fx_0");
+            cfgMap.put("clientMustReg","false");*/
         } catch (Exception e) {
             e.printStackTrace();
         }
